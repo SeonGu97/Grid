cd5fd76f5abf9ddbb798a7b47702db611d22020c
Kang Seon Gu <seongu97@naver.com>
2023-06-13 23:59:02 +0900

    Feat: full page(미완성)

--- javascript/event/event.js
+++ javascript/event/event.js
@@ -7,40 +7,42 @@ const event = (index, storage_data) => {
 
   const liLength = li.length - 1;
   const menuHeight = menu.getBoundingClientRect().height;
-  let minLength = 1;
+  let minLength = 0;
 
   const scroll = JSON.parse(localStorage.getItem(storage_data[0].key));
-  console.log(scroll);
 
   main.addEventListener("mousewheel", (e) => {
     const target = e.target;
 
-    e.deltaY > 0
-      ? moveScroll(menu, minLength, scroll, storage_data)
-      : moveScroll(menu, minLength, scroll, storage_data);
-
-    if (minLength > liLength) {
+    if (minLength < 0) {
+      minLength = 0;
+    } else if (minLength > liLength) {
       return (minLength = liLength);
     }
+
+    if (scroll[0] == false) {
+      if (minLength == 0) {
+        minLength = 1;
+      }
+    } else {
+      if (minLength == liLength) {
+        minLength = liLength - 1;
+      }
+    }
+
+    e.deltaY > 0
+      ? moveScroll(menu, minLength, scroll, storage_data, false)
+      : moveScroll(menu, minLength, scroll, storage_data, true);
   });
 
   menu.addEventListener("transitionend", (e) => {
     const target = e.target;
 
-    let boolean;
-
     if (scroll[0]) {
       minLength--;
-      boolean = true;
     } else {
       minLength++;
-      boolean = false;
     }
-
-    scroll.shift();
-    scroll.push(boolean);
-
-    localStorage.setItem(storage_data[0].key, JSON.stringify(scroll));
   });
 };
 
@@ -50,4 +52,11 @@ function moveScroll(menu, minLength, scroll, storage_data, boolean) {
   let height = menu.getBoundingClientRect().height;
 
   menu.style.transform = `translateY(-${height * minLength}px)`;
+
+  scroll.shift();
+  scroll.push(boolean);
+
+  // console.log(minLength);
+
+  localStorage.setItem(storage_data[0].key, JSON.stringify(scroll));
 }
